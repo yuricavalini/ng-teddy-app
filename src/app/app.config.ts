a54a2084e77616ab9a4d45a5034ff846cc677024
Yuri Cavalini <yuricavalini@gmail.com>
2025-03-16 16:00:06 -0300

    feat(ng-teddy-app): adiciona configuração para loading service e loading interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { registerLocaleData } from '@angular/common';
 import {
+  HTTP_INTERCEPTORS,
   provideHttpClient,
   withFetch,
   withInterceptorsFromDi,
@@ -7,6 +8,7 @@ import {
 import localePt from '@angular/common/locales/pt';
 import {
   ApplicationConfig,
+  importProvidersFrom,
   LOCALE_ID,
   provideZoneChangeDetection,
 } from '@angular/core';
@@ -15,6 +17,9 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 
 import { APP_ROUTES } from './app.routes';
+import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
+import { LoadingService } from './shared/services/loading.service';
+
 registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
@@ -25,5 +30,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
     provideAnimations(),
     provideToastr(),
+    importProvidersFrom(LoadingService),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
+import { authGuard } from './auth/guards/auth.guard';
+import { loggedInGuard } from './auth/guards/logged-in.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { ShellComponent } from './layout/shell/shell.component';
-import { loggedInGuard } from './auth/guards/logged-in.guard';
-import { authGuard } from './auth/guards/auth.guard';
 
 export const APP_ROUTES: Routes = [
   {
