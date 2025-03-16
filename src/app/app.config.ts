e6b13fd4c755e3a7ceab89ad1ed6d23e1bb94871
Yuri Cavalini <yuricavalini@gmail.com>
2025-03-16 14:24:49 -0300

    feat(ng-teddy-app): adiciona configurações basicas do app e ui libs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(APP_ROUTES, withComponentInputBinding()),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideAnimations(),
+    provideToastr(),
   ],
 };
